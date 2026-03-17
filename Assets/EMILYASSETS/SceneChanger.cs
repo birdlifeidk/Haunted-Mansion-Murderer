3fe7b8f3985999e28aee0d992f3051ee41815d6b
birdlifeidk <emiskimo@gmail.com>
2026-03-17 15:46:43 -0400

    LATTER DAY SAINTS
    Added scene changer, new ghost, fixed script

---
+++ Assets/EMILYASSETS/SceneChanger.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+
+ public void LoadNextScene()
+    {
+        SceneManager.LoadScene("DayAfter"); 
+    }
+}
